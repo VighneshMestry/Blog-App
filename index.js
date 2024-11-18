cd64670d2443ff2407bf4d421dffefe6f4ff3219
Vighnesh Mestry <mestryvighnesh27@gmail.com>
2024-11-18 23:03:21 +0530

    Setting up the features

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
-import 'dotenv/config';
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config();
 
-app.listen(process.env.PORT, () => console.log("Server Started"));
+app.set('view engine', 'ejs');
+app.set("views", path.resolve("./views"));
+
+app.get("/", (req, res) => {
+    return res.render("home");
+});
+
+mongoose.connect("mongodb://127.0.0.1:27017/blog-app");
+app.listen(process.env.PORT, () => console.log(`Server started at PORT: ${process.env.PORT}`));
---
+++ models/userModel.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const { createHmac, randomBytes } = require("node:crypto");
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    salt: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+    profileImageUrl: {
+        type: String,
+        default: "/images/user.png"
+    }
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) return;
+
+  const salt = randomBytes(16).toString();
+  const hashedPassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest("hex");
+
+  this.password = hashedPassword;
+  this.salt = salt;
+  next();
+});
+
+const User = mongoose.model("user", userSchema);
